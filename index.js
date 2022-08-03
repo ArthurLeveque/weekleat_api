9ba345cc78bb7cf42b72b55f1b60d1b13d205c2b
ArthurLeveque <arthur.lev44@gmail.com>
2022-08-04 00:10:08 +0200

    Config firebase + test connexion

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require('http')
+const http = require('http')
+const express = require('express');
+const app = express();
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase_config/weekleat-firebase-adminsdk.json');
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"})
-  response.end("Hello World with index.js !\n")
-}).listen(process.env.PORT)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const recipesRoute = require('./routes/recipesController');
+
+app.use('/recipes', recipesRoute);
+
+app.listen(5500, console.log('API ready !'));
+
+
+// *~~~~~~~~~~~~~~SERVIRA POUR MISE EN PROD SUR HEROKU~~~~~~~~~~~~~~*
+
+// http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"})
+//   response.end("Hello World with index.js !\n")
+// }).listen(process.env.PORT)
